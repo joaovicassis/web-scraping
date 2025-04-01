a614c04cc9772e8bae591e1a7590bb48fe60a3d2
Joao Assis <joaovictorassis03@gmail.com>
2025-04-01 16:17:21 -0300

    Initial commit: main function add

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from src.scraper import download_ans_files
+from src.processor import process_pdf_data
+
+def main():
+    try:
+        print("Iniciando processo de coleta e processamento de dados...")
+        
+
+        print("\n1. Fazendo download dos arquivos PDF...")
+        pdf_zip_path = download_ans_files()
+        
+
+        print("\n2. Processando dados do PDF...")
+        final_zip_path = process_pdf_data(pdf_zip_path)
+        
+        print("\nProcesso concluído com sucesso!")
+        print(f"Arquivo final gerado: {final_zip_path}")
+        
+    except Exception as e:
+        print(f"\nErro durante o processamento: {str(e)}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main() 
